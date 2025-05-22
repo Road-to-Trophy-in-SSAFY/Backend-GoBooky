4a938cd13594593eee1fcc91697e8683ab88eefe
김수민 <gkgkgkgk22@naver.com>
2025-05-22 12:44:13 +0900

    feat: Category, Book 모델 생성 및 초기 마이그레이션

--- books/models.py
+++ books/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name="books"
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    isbn = models.CharField(max_length=20)
+    cover = models.URLField()
+    publisher = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    author = models.CharField(max_length=100)
+    author_info = models.TextField()
+    author_photo = models.URLField()
+    customer_review_rank = models.FloatField()
+    subTitle = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
