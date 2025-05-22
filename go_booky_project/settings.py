32a10e685775526d246f37950afc1d703b42b6e0
김수민 <gkgkgkgk22@naver.com>
2025-05-22 16:22:14 +0900

    fix: CORS 허용 Domain 주소 오타 수정

--- go_booky_project/settings.py
+++ go_booky_project/settings.py
@@ -58,8 +58,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = "go_booky_project.urls"
