cf2faf13314fce1006ce054effc3c9137066fd14
Hyeseong Ro <nks3319@gmail.com>
2025-05-23 03:58:58 +0900

    feat: CustomUser 및 Category 모델 관리자 등록 및 설정 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,15 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Category
 
 
-# Register your models here.
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    pass
+    model = CustomUser
+    list_display = ("id", "email", "username", "is_active", "is_staff")
+    search_fields = ("email", "username")
+    ordering = ("id",)
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ()}),)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
