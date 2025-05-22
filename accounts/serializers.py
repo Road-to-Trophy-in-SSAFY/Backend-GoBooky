6f009a2d5f78709581ff21c1e7845ecf49be32bf
Hyeseong Ro <nks3319@gmail.com>
2025-05-23 03:59:33 +0900

    feat: 사용자 등록, 이메일 인증 및 프로필 완성을 위한 직렬 변환기 추가

---
+++ accounts/serializers.py
@@ -0,0 +1,66 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    username = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("email", "password", "username")
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("이미 가입된 이메일입니다.")
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+            username=validated_data["username"],
+            is_active=False,  # 이메일 인증 전까지 비활성
+        )
+        return user
+
+
+class VerifyEmailSerializer(serializers.Serializer):
+    uuid = serializers.CharField()
+
+
+class ProfileCompleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name")
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(email=data["email"], password=data["password"])
+        if not user:
+            raise serializers.ValidationError(_("이메일 혹은 비밀번호가 틀렸습니다."))
+        if not user.is_active:
+            raise serializers.ValidationError(_("이메일 인증이 필요합니다."))
+        data["user"] = user
+        return data
+
+
+class AccountDeleteSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True)
+
+    def validate_password(self, value):
+        user = self.context["request"].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("비밀번호가 일치하지 않습니다.")
+        return value
