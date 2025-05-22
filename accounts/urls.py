0f555569f4c97835e04cb9df406f5f8e008ee2b5
Hyeseong Ro <nks3319@gmail.com>
2025-05-23 00:05:02 +0900

    feat: 카테고리 목록 API 추가 및 CategoryListView 클래스 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
+    # 카테고리 목록 API - 회원가입 시 필요
+    path("categories/", views.CategoryListView.as_view(), name="category-list"),
     # 필요한 추가 API 엔드포인트를 이곳에 정의
     # 예시: path('profile/', views.UserProfileView.as_view(), name='user-profile'),
 ]
