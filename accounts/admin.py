e4152f1df97a45d4d57504a70984ab85556bd174
Hyeseong Ro <nks3319@gmail.com>
2025-05-23 01:04:29 +0900

    refactor: accounts 앱의 코드 초기화

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,35 +5,4 @@ from .models import CustomUser, Category
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
-    # UserAdmin 커스터마이징
-    fieldsets = UserAdmin.fieldsets + (
-        (
-            "추가 정보",
-            {
-                "fields": (
-                    "gender",
-                    "age",
-                    "weekly_avg_reading_time",
-                    "annual_reading_count",
-                    "profile_image",
-                    "categories",
-                )
-            },
-        ),
-    )
-    list_display = (
-        "username",
-        "email",
-        "first_name",
-        "last_name",
-        "gender",
-        "age",
-        "annual_reading_count",
-    )
-    list_filter = UserAdmin.list_filter + ("gender", "age")
-    search_fields = UserAdmin.search_fields + ("gender", "age", "annual_reading_count")
-
-
-# 모델 등록
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Category)
+    pass
