5dc32d4becaaba48e8f742c7e0fc8b37b70de4f8
HyeseongRo <nks3319@gmail.com>
2025-05-22 17:54:32 +0900

    feat: 미디어 파일 서빙 설정 추가, 개발환경에서의 파일 서빙 지원

--- go_booky_project/urls.py
+++ go_booky_project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+# 미디어 파일 서빙 설정 (개발환경에서만)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,9 +28,15 @@ urlpatterns = [
     path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
     # allauth 소셜 로그인 시 필요한 endpoint
     path("accounts/social/", include("allauth.urls")),
+    # accounts 앱 API 엔드포인트
+    # path("api/accounts/", include("accounts.urls")),
 ]
 
 # browsable API에서 로그인/로그아웃 지원
 urlpatterns += [
     path("api-auth/", include("rest_framework.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
